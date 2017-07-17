7350c82e57013cd0914e1ad831275977439f6c26
wonwoo <aoruqjfu@gmail.com>
2017-07-17 21:15:32 +0900

    RefreshScope 추가

---
+++ user-service/src/main/java/me/wonwoo/HelloProperties.java
@@ -0,0 +1,19 @@
+package me.wonwoo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Created by wonwoolee on 2017. 7. 17..
+ */
+@ConfigurationProperties("hello")
+public class HelloProperties {
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
--- user-service/src/main/java/me/wonwoo/UserServiceApplication.java
+++ user-service/src/main/java/me/wonwoo/UserServiceApplication.java
@@ -2,7 +2,9 @@ package me.wonwoo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,7 @@ import java.util.List;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigurationProperties(HelloProperties.class)
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
@@ -21,7 +24,14 @@ public class UserServiceApplication {
 
 
 @RestController
+@RefreshScope
 class UserRestController {
+
+	private final HelloProperties helloProperties;
+
+	UserRestController(HelloProperties helloProperties) {
+		this.helloProperties = helloProperties;
+	}
 
 	@GetMapping("/users")
 	public List<User> getUesrs() {
@@ -32,6 +42,13 @@ class UserRestController {
 	public User getUser(@PathVariable String name) {
 		return new User(name);
 	}
+
+
+	@GetMapping("/hello")
+	public String hello() {
+		return helloProperties.getName();
+	}
+
 }
 
 
